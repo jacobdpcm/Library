f233b04ef578b475477c27d222603435371f889e
JacobPCM <jacobd@postcardmania.com>
2024-02-07 17:42:20 -0500

    Establish form skeleton

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
+Book.prototype.info = function(){
+    return this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + read;
+}
+
+const newBook = document.querySelector('.new');
+const form = document.querySelector('form');
+newBook.addEventListener('click', () => form.classList.toggle('hidden'))
