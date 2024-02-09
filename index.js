29ff9e7b9ac86a3e28e57846e02ec30a8ccca356
JacobPCM <jacobd@postcardmania.com>
2024-02-09 17:15:16 -0500

    Set up library logic and some styling

--- index.js
+++ index.js
@@ -9,6 +9,63 @@ Book.prototype.info = function(){
     return this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + read;
 }
 
-const newBook = document.querySelector('.new');
+function addBook(libraryArray){
+  const title = document.querySelector('#title');
+  const author = document.querySelector('#author');
+  const pages = document.querySelector('#pages');
+  const read = document.querySelector('#read');
+
+  const theBook = new Book(title.value, author.value, pages.value, read.value)
+  libraryArray.push(theBook);
+}
+
+function updateLibrary(libraryArray){
+  clearLibrary();
+
+  console.log(library)
+
+  libraryArray.forEach(element => {
+    const library = document.querySelector('.library');
+    const bookspot = document.createElement('div');
+    bookspot.classList.add('example');
+    library.appendChild(bookspot);
+
+    const title = document.createElement('div');
+    title.textContent = element.title;
+    bookspot.appendChild(title);
+
+    const author = document.createElement('div');
+    author.textContent = "By" + element.author;
+    bookspot.appendChild(author);
+
+    const pages = document.createElement('div');
+    pages.textContent = element.pages + "Pages";
+    bookspot.appendChild(pages);
+
+    const read = document.createElement('div');
+    read.textContent = element.read;
+    bookspot.appendChild(read);
+    
+  });
+}
+
+function clearLibrary(){
+  const library = document.querySelector('.library');
+  library.innerHTML = ''
+}
+
+
+let library = [];
+
+const newButton = document.querySelector('.new');
 const form = document.querySelector('form');
-newBook.addEventListener('click', () => form.classList.toggle('hidden'))
+const confirmButton = document.querySelector('.confbut');
+
+newButton.addEventListener('click', () => form.classList.toggle('hidden'));
+
+confirmButton.addEventListener('click', () => {
+  addBook(library);
+  updateLibrary(library);
+});
+
+
